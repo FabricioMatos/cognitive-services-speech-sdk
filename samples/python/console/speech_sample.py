9955e08df2369f0aa4fe0d9795b6775ed6319b25
Fabricio Vargas Matos <fabriciovargasmatos@gmail.com>
2019-03-28 02:41:59 -0300

    continuous recognition working

--- samples/python/console/speech_sample.py
+++ samples/python/console/speech_sample.py
@@ -37,29 +37,38 @@ def speech_continuous_recognition_async_from_mic():
     """performs continuous speech recognition from the default microphone"""
     # <SpeechRecognitionWithMicrophone>
     speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+    
+    # TODO: The device_name isn't correct
+    # https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/how-to-select-audio-input-devices
+    # audio_config = speechsdk.audio.AudioConfig(device_name="Built-in Microphone")
+    # audio_config = speechsdk.audio.AudioConfig(device_name="Loopback Audio")
+    
     # The default language is "en-us".
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
-    # Starts speech recognition, and returns after a single utterance is recognized. The end of a
-    # single utterance is determined by listening for silence at the end or until a maximum of 15
-    # seconds of audio is processed. It returns the recognition text as result.
-    # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
-    # shot recognition like command or query.
-    # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
-    result = speech_recognizer.recognize_once()
-
-    # Check the result
-    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("Recognized: {}".format(result.text))
-    elif result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized")
-    elif result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-    # </SpeechRecognitionWithMicrophone>
-
+    done = False
+ 
+    def stop_cb(evt):
+        """callback that stops continuous recognition upon receiving an event `evt`"""
+        print('CLOSING on {}'.format(evt))
+        speech_recognizer.stop_continuous_recognition()
+        nonlocal done
+        done = True
+ 
+    # Connect callbacks to the events fired by the speech recognizer
+    speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
+    speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
+    speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+    speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+    speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+    # stop continuous recognition on either session stopped or canceled events
+    speech_recognizer.session_stopped.connect(stop_cb)
+    speech_recognizer.canceled.connect(stop_cb)
+ 
+    # Start continuous speech recognition
+    speech_recognizer.start_continuous_recognition()
+    while not done:
+        time.sleep(.5)
 
 def speech_recognize_once_from_mic():
     """performs one-shot speech recognition from the default microphone"""
